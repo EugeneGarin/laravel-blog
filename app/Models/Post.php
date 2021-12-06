ef3c34a0594626982fd25c53709a79e60e3c2d70
Eugene <diterecs@gmail.com>
2021-12-06 21:52:14 +0200

    #24 - added category model and migration

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,4 +10,9 @@ class Post extends Model
     use HasFactory;
 
     protected $fillable = ['title', 'body', 'slug', 'excerpt'];
+
+    public function Category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
